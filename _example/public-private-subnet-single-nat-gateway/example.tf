ae4deda6ca0205b3a4cc0ac2081d224b87b25b63
Anmol Nagpal <ianmolnagpal@gmail.com>
2021-06-23 14:30:50 -0400

    fixing example

--- _example/public-private-subnet-single-nat-gateway/example.tf
+++ _example/public-private-subnet-single-nat-gateway/example.tf
@@ -7,7 +7,7 @@ module "vpc" {
   version = "0.15.0"
 
   name        = "vpc"
-  environment = "test"
+  environment = "sandbox"
   label_order = ["name", "environment"]
 
   cidr_block = "10.0.0.0/16"
@@ -16,16 +16,18 @@ module "vpc" {
 module "subnets" {
   source = "./../../"
 
+  nat_gateway_enabled = true
+  single_nat_gateway  = false
+
   name        = "subnets"
-  environment = "test"
+  environment = "sandbox"
   label_order = ["name", "environment"]
 
   availability_zones  = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
   vpc_id              = module.vpc.vpc_id
   type                = "public-private"
   igw_id              = module.vpc.igw_id
-  nat_gateway_enabled = true
   cidr_block          = module.vpc.vpc_cidr_block
   ipv6_cidr_block     = module.vpc.ipv6_cidr_block
-  single_nat_gateway  = true
+
 }
