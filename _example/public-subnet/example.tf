d4b3fd20bcde645b3be80f347733af6ef372c952
theprashantyadav <79893616+theprashantyadav@users.noreply.github.com>
2023-06-06 21:41:27 +0530

    feat: update resource and readme.yaml (#43)
    Co-authored-by: anmolnagpal <anmol@clouddrove.com>

--- _example/public-subnet/example.tf
+++ _example/public-subnet/example.tf
@@ -1,10 +1,16 @@
+####----------------------------------------------------------------------------------
+## Provider block added, Use the Amazon Web Services (AWS) provider to interact with the many resources supported by AWS.
+####----------------------------------------------------------------------------------
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-west-1"
 }
 
+####----------------------------------------------------------------------------------
+## A VPC is a virtual network that closely resembles a traditional network that you'd operate in your own data center.
+####----------------------------------------------------------------------------------
 module "vpc" {
   source  = "clouddrove/vpc/aws"
-  version = "1.3.0"
+  version = "1.3.1"
 
   name        = "vpc"
   environment = "test"
@@ -13,6 +19,9 @@ module "vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
+####----------------------------------------------------------------------------------
+## Subnet is a range of IP addresses in your VPC.
+####----------------------------------------------------------------------------------
 module "subnets" {
   source = "./../../"
 
@@ -20,12 +29,11 @@ module "subnets" {
   environment = "test"
   label_order = ["name", "environment"]
 
-  availability_zones              = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  vpc_id                          = module.vpc.vpc_id
-  type                            = "public"
-  igw_id                          = module.vpc.igw_id
-  cidr_block                      = module.vpc.vpc_cidr_block
-  ipv6_cidr_block                 = module.vpc.ipv6_cidr_block
-  assign_ipv6_address_on_creation = false
+  availability_zones = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  vpc_id             = module.vpc.vpc_id
+  type               = "public"
+  igw_id             = module.vpc.igw_id
+  cidr_block         = module.vpc.vpc_cidr_block
+  ipv6_cidr_block    = module.vpc.ipv6_cidr_block
 
 }
