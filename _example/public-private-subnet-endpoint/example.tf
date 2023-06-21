9fe72e86806c1bad2e7bf5a7271b148592277c3c
vibhuti goyal <vibhutigoyal.vg78@gmail.com>
2023-06-21 23:05:50 +0530

    feat: enabled vpc endpoint

--- _example/public-private-subnet-endpoint/example.tf
+++ _example/public-private-subnet-endpoint/example.tf
@@ -41,5 +41,5 @@ module "subnets" {
   assign_ipv6_address_on_creation = false
   enable_vpc_endpoint             = true
   service_name                    = "com.amazonaws.${data.aws_region.current.name}.ec2"
-  enable_flow_log = false
+  enable_flow_log = true
 }
