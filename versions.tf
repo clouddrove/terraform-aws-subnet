bb20e6059326f5ae1b7b9cc112fc7d71096ce1a5
Archit Chopra <87892837+13archit@users.noreply.github.com>
2023-07-27 22:31:38 +0530

    fix: Fixed Readme.yaml & Versions.tf (#50)

--- versions.tf
+++ versions.tf
@@ -1,11 +1,11 @@
 # Terraform version
 terraform {
-  required_version = ">= 1.4.6"
+  required_version = ">= 1.5.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.1.0"
+      version = ">= 5.9.0"
     }
   }
 }
